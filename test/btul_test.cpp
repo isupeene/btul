18afb5ceb434f56fa43b2ccbd242c70947917568
Isaac Supeene <isupeene@ualberta.ca>
2014-05-03 20:18:31 +0000

    Added a base test class for testing output stream operators, and added a basic test for lengths.

--- test/btul_test.cpp
+++ test/btul_test.cpp
@@ -1,7 +1,19 @@
-#include "gtest/gtest.h"
+#include <gtest/gtest.h>
 #include <btul.h>
+#include <BaseTestCases.h>
+#include <ostream>
 
-TEST(btul_test, test00_pass) {
-	// TODO
+
+typedef OutputStreamTest HelloTest;
+
+TEST_F(HelloTest, test00_pass) {
+	expected = "Hello!";
+	result << "Hello!";
+}
+
+TEST(ValueTests, test00_meters) {
+	double expected = 10.0;
+	Length actual = 10.0_m;
+	ASSERT_EQ(expected, actual.value);
 }
 
