5d6bc7129fb68d2f21c6893026d7e97c4a414c11
Isaac Supeene <isupeene@ualberta.ca>
2014-05-11 18:06:12 +0000

    Added a few test cases, and fixed a grievous defect with exponents.

--- src/btul.h
+++ src/btul.h
@@ -28,33 +28,6 @@ SOFTWARE. */
 #include <string>
 #include <sstream>
 
-template <int i>
-class Power {
-public:
-	constexpr Power() {}
-};
-
-#define DEFINE_POWER(VALUE)	\
-constexpr Power<VALUE> p##VALUE
-
-#define DEFINE_NEGATIVE_POWER(VALUE)	\
-constexpr Power<-VALUE> n##VALUE
-
-
-#define DEFINE_POWERS(VALUE)	\
-DEFINE_POWER(VALUE);		\
-DEFINE_NEGATIVE_POWER(VALUE)
-
-DEFINE_POWERS(0);
-DEFINE_POWERS(1);
-DEFINE_POWERS(2);
-DEFINE_POWERS(3);
-DEFINE_POWERS(4);
-DEFINE_POWERS(5);
-DEFINE_POWERS(6);
-DEFINE_POWERS(7);
-DEFINE_POWERS(8);
-DEFINE_POWERS(9);
 
 #define BASE_QUANTITIES_DECLARATION	\
 	int Length,			\
@@ -111,14 +84,16 @@ DEFINE_POWERS(9);
 #define BASE_QUANTITIES_ADD BASE_QUANTITIES_OP(+)
 #define BASE_QUANTITIES_SUB BASE_QUANTITIES_OP(-)
 
-#define BASE_QUANTITIES_MUL(VALUE)	\
-	Length * VALUE,			\
-	Mass * VALUE,			\
-	Time * VALUE,			\
-	Current * VALUE,		\
-	Temperature * VALUE,		\
-	Amount * VALUE,			\
-	Luminosity * VALUE
+#define BASE_QUANTITIES_N_MUL(N, VALUE)	\
+	Length##N * VALUE,		\
+	Mass##N * VALUE,		\
+	Time##N * VALUE,		\
+	Current##N * VALUE,		\
+	Temperature##N * VALUE,		\
+	Amount##N * VALUE,		\
+	Luminosity##N * VALUE
+
+#define BASE_QUANTITIES_1_MUL(VALUE) BASE_QUANTITIES_N_MUL(1, VALUE)
 
 namespace detail {
 	const char* super(char c) {
@@ -281,11 +256,12 @@ public:
 };
 
 
+#define OP_RESULT_TYPE(T1, OP, T2) decltype(std::declval<T1>() OP std::declval<T2>())
 #define POW_TYPE(T1, T2) decltype(std::pow(std::declval<T1>(), std::declval<T2>()))
 
-template <BASE_QUANTITIES_DECLARATION,
+template <BASE_QUANTITIES_DECLARATION_1,
 	  class Number = long double,
-	  class Format = DefaultQuantityFormat<BASE_QUANTITIES>>
+	  class Format = DefaultQuantityFormat<BASE_QUANTITIES_1>>
 class Quantity {
 public:
 	constexpr Quantity() {}
@@ -294,48 +270,145 @@ public:
 	{}
 
 	template <class T, class F>
-	constexpr Quantity(Quantity<BASE_QUANTITIES, T, F> other)
-		: value(other.value)
+	constexpr Quantity(Quantity<BASE_QUANTITIES_1, T, F> other)
+		: value(other.Value())
 	{}
 
 	template <class T, class F>
-	constexpr Quantity& operator =(Quantity<BASE_QUANTITIES, T, F> other) {
+	constexpr Quantity& operator =(Quantity<BASE_QUANTITIES_1, T, F> other) {
 		this->value = other.value;
 		return *this;
 	}
 
-	template <int x>
-	constexpr Quantity<BASE_QUANTITIES_MUL(x), POW_TYPE(Number, int)>
-	operator [](Power<x>) {
-		return Quantity<BASE_QUANTITIES_MUL(x),
-				POW_TYPE(Number, int)>
-		       (std::pow(value, x));
+	#define DECLARE_POWER(N)						\
+	constexpr Quantity<BASE_QUANTITIES_1_MUL(N), Number, Format> p##N() {	\
+		return Quantity<BASE_QUANTITIES_1_MUL(N), Number, Format>(	\
+			std::pow(value, N)					\
+		);								\
+	}									\
+										\
+	constexpr Quantity<BASE_QUANTITIES_1_MUL(-N), Number, Format> n##N() {	\
+		return Quantity<BASE_QUANTITIES_1_MUL(-N), Number, Format>(	\
+			std::pow(value, -N)					\
+		);								\
 	}
+
+	DECLARE_POWER(0);
+	DECLARE_POWER(1);
+	DECLARE_POWER(2);
+	DECLARE_POWER(3);
+	DECLARE_POWER(4);
+	DECLARE_POWER(5);
+	DECLARE_POWER(6);
+	DECLARE_POWER(7);
+	DECLARE_POWER(8);
+	DECLARE_POWER(9);
+
+	#undef DECLARE_POWER
 
 	template <class NewFormat>
-	constexpr Quantity<BASE_QUANTITIES, Number, NewFormat> withFormat() {
-		return Quantity<BASE_QUANTITIES, Number, NewFormat>(value);
+	constexpr Quantity<BASE_QUANTITIES_1, Number, NewFormat> withFormat() {
+		return Quantity<BASE_QUANTITIES_1, Number, NewFormat>(value);
 	}
 
-	const Number value;
+	constexpr Number Value() {
+		return value;
+	}
 
-	static constexpr int length = Length;
-	static constexpr int mass = Mass;
-	static constexpr int time = Time;
-	static constexpr int temperature = Temperature;
-	static constexpr int current = Current;
-	static constexpr int amount = Amount;
-	static constexpr int luminosity = Luminosity;
+	static constexpr int length = Length1;
+	static constexpr int mass = Mass1;
+	static constexpr int time = Time1;
+	static constexpr int temperature = Temperature1;
+	static constexpr int current = Current1;
+	static constexpr int amount = Amount1;
+	static constexpr int luminosity = Luminosity1;
+
+	typedef Number type;
+	typedef Format format;
+
+protected:
+	Number value;
+
+private:
+	template <BASE_QUANTITIES_DECLARATION,
+		  class T1, class F1,
+		  class T2, class F2>
+	friend constexpr Quantity<BASE_QUANTITIES, T1, F1>& operator +=(
+		Quantity<BASE_QUANTITIES, T1, F1>&,
+		const Quantity<BASE_QUANTITIES, T2, F2>&
+	);
+
+	template <BASE_QUANTITIES_DECLARATION,
+		  class T1, class F1,
+		  class T2, class F2>
+	friend constexpr Quantity<BASE_QUANTITIES, T1, F1>& operator -=(
+		Quantity<BASE_QUANTITIES, T1, F1>&,
+		const Quantity<BASE_QUANTITIES, T2, F2>&
+	);
+
+	template <BASE_QUANTITIES_DECLARATION, class T1, class F, class T2>
+	friend constexpr Quantity<BASE_QUANTITIES, T1, F>& operator *=(
+		Quantity<BASE_QUANTITIES, T1, F>&,
+		const T2&
+	);
+
+	template <BASE_QUANTITIES_DECLARATION, class T1, class F, class T2>
+	friend constexpr Quantity<BASE_QUANTITIES, T1, F>& operator /=(
+		Quantity<BASE_QUANTITIES, T1, F>&,
+		const T2&
+	);
+
+	template <BASE_QUANTITIES_DECLARATION, class T1, class F, class T2>
+	friend constexpr Quantity<BASE_QUANTITIES, T1, F>& operator %=(
+		Quantity<BASE_QUANTITIES, T1, F>&,
+		const T2&
+	);
+
+	template <BASE_QUANTITIES_DECLARATION, class T, class F>
+	friend constexpr Quantity<BASE_QUANTITIES, T, F>& operator ++(
+		Quantity<BASE_QUANTITIES, T, F>&
+	);
+
+	template <BASE_QUANTITIES_DECLARATION, class T, class F>
+	friend constexpr Quantity<BASE_QUANTITIES, T, F>& operator --(
+		Quantity<BASE_QUANTITIES, T, F>&
+	);
+
+	template <BASE_QUANTITIES_DECLARATION, class T, class F>
+	friend constexpr Quantity<BASE_QUANTITIES, T, F> operator ++(
+		Quantity<BASE_QUANTITIES, T, F>&,
+		int
+	);
+
+	template <BASE_QUANTITIES_DECLARATION, class T, class F>
+	friend constexpr Quantity<BASE_QUANTITIES, T, F> operator --(
+		Quantity<BASE_QUANTITIES, T, F>&,
+		int
+	);
 };
 
+// We use some cheap tricks with the comma operator here, since C++11
+// only supports single statements as constexpr function bodies.
 
-#define OP_RESULT_TYPE(T1, OP, T2) decltype(std::declval<T1>() OP std::declval<T2>())
-
-#define DECLARE_ADDITIVE_QUANTITY_OPERATOR(OP)						  \
-template <BASE_QUANTITIES_DECLARATION, class T1, class F1, class T2, class F2>		  \
-constexpr Quantity<BASE_QUANTITIES, OP_RESULT_TYPE(T1, OP, T2)>				  \
-operator OP(Quantity<BASE_QUANTITIES, T1, F1> x, Quantity<BASE_QUANTITIES, T2, F2> y) {	  \
-	return Quantity<BASE_QUANTITIES, OP_RESULT_TYPE(T1, OP, T2)>(x.value OP y.value); \
+#define DECLARE_ADDITIVE_QUANTITY_OPERATOR(OP)					\
+template <BASE_QUANTITIES_DECLARATION,						\
+	  class T1, class F1,							\
+	  class T2, class F2>							\
+constexpr Quantity<BASE_QUANTITIES, OP_RESULT_TYPE(T1, OP, T2)>			\
+operator OP(const Quantity<BASE_QUANTITIES, T1, F1>& x,				\
+	    const Quantity<BASE_QUANTITIES, T2, F2>& y) 			\
+{										\
+	return Quantity<BASE_QUANTITIES, OP_RESULT_TYPE(T1, OP, T2)>(		\
+		x.Value() OP y.Value()						\
+	);									\
+}										\
+										\
+template <BASE_QUANTITIES_DECLARATION, class T1, class F1, class T2, class F2>	\
+constexpr Quantity<BASE_QUANTITIES, T1, F1>&					\
+operator OP##=(Quantity<BASE_QUANTITIES, T1, F1>& x,				\
+	       const Quantity<BASE_QUANTITIES, T2, F2>& y)			\
+{										\
+	return (x.value OP##= y.Value(), x);					\
 }
 
 DECLARE_ADDITIVE_QUANTITY_OPERATOR(+)
@@ -345,33 +418,36 @@ DECLARE_ADDITIVE_QUANTITY_OPERATOR(-)
 template <BASE_QUANTITIES_DECLARATION_1, class T1, class F1,				\
 	  BASE_QUANTITIES_DECLARATION_2, class T2, class F2>				\
 constexpr Quantity<BASE_QUANTITIES_OP(UNIT_OP), OP_RESULT_TYPE(T1, OP, T2)>		\
-operator OP(Quantity<BASE_QUANTITIES_1, T1, F1> x,					\
-	    Quantity<BASE_QUANTITIES_2, T2, F2> y)					\
+operator OP(const Quantity<BASE_QUANTITIES_1, T1, F1>& x,				\
+	    const Quantity<BASE_QUANTITIES_2, T2, F2>& y)				\
 {											\
-	return Quantity<BASE_QUANTITIES_OP(UNIT_OP), OP_RESULT_TYPE(T1, OP, T2)>	\
-	       (x.value OP y.value);							\
+	return Quantity<BASE_QUANTITIES_OP(UNIT_OP), OP_RESULT_TYPE(T1, OP, T2)>(	\
+		x.Value() OP y.Value()							\
+	);										\
 }											\
 											\
 template <BASE_QUANTITIES_DECLARATION, class T1, class F, class T2>			\
 constexpr Quantity<BASE_QUANTITIES, OP_RESULT_TYPE(T1, OP, T2), F>			\
-operator OP(Quantity<BASE_QUANTITIES, T1, F> x, T2 y) {					\
-	return Quantity<BASE_QUANTITIES, OP_RESULT_TYPE(T1, OP, T2), F>(x.value OP y);	\
+operator OP(const Quantity<BASE_QUANTITIES, T1, F>& x, const T2& y) {			\
+	return Quantity<BASE_QUANTITIES, OP_RESULT_TYPE(T1, OP, T2), F>(		\
+		x.Value() OP y								\
+	);										\
+}											\
+											\
+template <BASE_QUANTITIES_DECLARATION, class T1, class F, class T2>			\
+constexpr Quantity<BASE_QUANTITIES, T1, F>&						\
+operator OP##=(Quantity<BASE_QUANTITIES, T1, F>& x, const T2& y) {			\
+	return (x.value OP##= y, x);							\
 }
 
 DECLARE_MULTIPLICATIVE_QUANTITY_OPERATOR(*, +)
 DECLARE_MULTIPLICATIVE_QUANTITY_OPERATOR(/, -)
 
-template <BASE_QUANTITIES_DECLARATION, class T1, class F, class T2>
-constexpr Quantity<BASE_QUANTITIES, OP_RESULT_TYPE(T1, %, T2), F>
-operator %(Quantity<BASE_QUANTITIES, T1, F> x, T2 y) {
-	return Quantity<BASE_QUANTITIES, OP_RESULT_TYPE(T1, %, T2), F>(x.value % y);
-}
-
 #define DECLARE_UNARY_QUANTITY_OPERATOR(OP)			\
 template <BASE_QUANTITIES_DECLARATION, class T, class F>	\
 constexpr Quantity<BASE_QUANTITIES, T, F>			\
 operator OP(Quantity<BASE_QUANTITIES, T, F> x) {		\
-	return Quantity<BASE_QUANTITIES, T, F>(OP x.value);	\
+	return Quantity<BASE_QUANTITIES, T, F>(OP x.Value());	\
 }
 
 DECLARE_UNARY_QUANTITY_OPERATOR(+)
@@ -381,8 +457,7 @@ DECLARE_UNARY_QUANTITY_OPERATOR(-)
 template <BASE_QUANTITIES_DECLARATION, class T, class F>	\
 constexpr Quantity<BASE_QUANTITIES, T, F>&			\
 operator OP(Quantity<BASE_QUANTITIES, T, F>& x) {		\
-	OP x.value;						\
-	return x;						\
+	return (OP x.value, x);					\
 }
 
 DECLARE_PREFIX_QUANTITY_OPERATOR(++)
@@ -392,9 +467,7 @@ DECLARE_PREFIX_QUANTITY_OPERATOR(--)
 template <BASE_QUANTITIES_DECLARATION, class T, class F>	\
 constexpr Quantity<BASE_QUANTITIES, T, F>			\
 operator OP(Quantity<BASE_QUANTITIES, T, F>& x, int) {		\
-	Quantity<BASE_QUANTITIES, T, F> result = x;		\
-	OP x.value;						\
-	return result;						\
+	return Quantity<BASE_QUANTITIES, T, F>(x.value OP);	\
 }
 
 DECLARE_POSTFIX_QUANTITY_OPERATOR(++)
@@ -404,7 +477,7 @@ DECLARE_POSTFIX_QUANTITY_OPERATOR(--)
 template <BASE_QUANTITIES_DECLARATION, class T1, class F1, class T2, class F2>		\
 constexpr bool										\
 operator OP(Quantity<BASE_QUANTITIES, T1, F1> x, Quantity<BASE_QUANTITIES, T2, F2> y) {	\
-	return x.value OP y.value;							\
+	return x.Value() OP y.Value();							\
 }
 
 DECLARE_QUANTITY_COMPARISON_OPERATOR(==)
@@ -414,11 +487,12 @@ DECLARE_QUANTITY_COMPARISON_OPERATOR(>=)
 DECLARE_QUANTITY_COMPARISON_OPERATOR(<)
 DECLARE_QUANTITY_COMPARISON_OPERATOR(<=)
 
-template <BASE_QUANTITIES_DECLARATION, class T, class Format, class ostream>
-ostream& operator <<(ostream& stream, Quantity<BASE_QUANTITIES, T, Format> quantity) {
-	return stream << Format::Format(quantity.value);
+template <BASE_QUANTITIES_DECLARATION, class T, class Format>
+std::ostream& operator <<(std::ostream& stream,
+			  const Quantity<BASE_QUANTITIES, T, Format>& quantity)
+{
+	return stream << Format::Format(quantity.Value());
 }
-
 
 
 #define DECLARE_MULTIPLIER(QUANTITY, UNIT, PREFIX, EXPONENT)			\
@@ -430,26 +504,110 @@ constexpr QUANTITY operator "" _##PREFIX##UNIT(unsigned long long value) {	\
 }										\
 constexpr QUANTITY PREFIX##UNIT = 1.0_##PREFIX##UNIT
 
+#define DECLARE_MULTIPLIERS(QUANTITY, UNIT)	\
+DECLARE_MULTIPLIER(QUANTITY, UNIT, T,  12);	\
+DECLARE_MULTIPLIER(QUANTITY, UNIT, G,  9);	\
+DECLARE_MULTIPLIER(QUANTITY, UNIT, M,  6);	\
+DECLARE_MULTIPLIER(QUANTITY, UNIT, k,  3);	\
+DECLARE_MULTIPLIER(QUANTITY, UNIT, h,  2);	\
+DECLARE_MULTIPLIER(QUANTITY, UNIT, da, 1);	\
+DECLARE_MULTIPLIER(QUANTITY, UNIT, d, -1);	\
+DECLARE_MULTIPLIER(QUANTITY, UNIT, c, -2);	\
+DECLARE_MULTIPLIER(QUANTITY, UNIT, m, -3);	\
+DECLARE_MULTIPLIER(QUANTITY, UNIT, u, -6);	\
+DECLARE_MULTIPLIER(QUANTITY, UNIT, n, -9);	\
+DECLARE_MULTIPLIER(QUANTITY, UNIT, p, -12)
+
+#define DECLARE_MULTIPLIER_POWER(QUANTITY, UNIT, PREFIX, EXPONENT, N)			\
+constexpr decltype(UNIT.p##N())								\
+operator "" _##PREFIX##UNIT##_p##N(long double value) {					\
+	return UNIT.p##N() * std::pow(10, EXPONENT * N) * value;			\
+}											\
+											\
+constexpr decltype(UNIT.p##N())								\
+operator "" _##PREFIX##UNIT##_p##N(long long unsigned value) {				\
+	return UNIT.p##N() * std::pow(10, EXPONENT * N) * value;			\
+}											\
+											\
+constexpr decltype(UNIT.n##N())								\
+operator "" _##PREFIX##UNIT##_n##N(long double value) {					\
+	return UNIT.n##N() * std::pow(10, EXPONENT * -N) * value;			\
+}											\
+											\
+constexpr decltype(UNIT.n##N())								\
+operator "" _##PREFIX##UNIT##_n##N(long long unsigned value) {				\
+	return UNIT.n##N() * std::pow(10, EXPONENT * -N) * value;			\
+}											\
+											\
+constexpr decltype(UNIT.p##N()) PREFIX##UNIT##_p##N = 1.0_##PREFIX##UNIT##_p##N;	\
+constexpr decltype(UNIT.n##N()) PREFIX##UNIT##_n##N = 1.0_##PREFIX##UNIT##_n##N
+
+#define DECLARE_MULTIPLIER_POWERS(QUANTITY, UNIT, N)	\
+DECLARE_MULTIPLIER_POWER(QUANTITY, UNIT, T,  12, N);	\
+DECLARE_MULTIPLIER_POWER(QUANTITY, UNIT, G,  9, N);	\
+DECLARE_MULTIPLIER_POWER(QUANTITY, UNIT, M,  6, N);	\
+DECLARE_MULTIPLIER_POWER(QUANTITY, UNIT, k,  3, N);	\
+DECLARE_MULTIPLIER_POWER(QUANTITY, UNIT, h,  2, N);	\
+DECLARE_MULTIPLIER_POWER(QUANTITY, UNIT, da, 1, N);	\
+DECLARE_MULTIPLIER_POWER(QUANTITY, UNIT, d, -1, N);	\
+DECLARE_MULTIPLIER_POWER(QUANTITY, UNIT, c, -2, N);	\
+DECLARE_MULTIPLIER_POWER(QUANTITY, UNIT, m, -3, N);	\
+DECLARE_MULTIPLIER_POWER(QUANTITY, UNIT, u, -6, N);	\
+DECLARE_MULTIPLIER_POWER(QUANTITY, UNIT, n, -9, N);	\
+DECLARE_MULTIPLIER_POWER(QUANTITY, UNIT, p, -12, N)
+
+
+#define DECLARE_POWER(QUANTITY, UNIT, N)				\
+constexpr decltype(UNIT.p##N())						\
+operator "" _##UNIT##_p##N(long double value) {				\
+	return UNIT.p##N() * value;					\
+}									\
+									\
+constexpr decltype(UNIT.p##N())						\
+operator "" _##UNIT##_p##N(long long unsigned value) {			\
+	return UNIT.p##N() * value;					\
+}									\
+									\
+constexpr decltype(UNIT.n##N())						\
+operator "" _##UNIT##_n##N(long double value) {				\
+	return UNIT.n##N() * value;					\
+}									\
+									\
+constexpr decltype(UNIT.n##N())						\
+operator "" _##UNIT##_n##N(long long unsigned value) {			\
+	return UNIT.n##N() * value;					\
+}									\
+									\
+constexpr decltype(UNIT.p##N())	UNIT##_p##N = 1.0_##UNIT##_p##N;	\
+constexpr decltype(UNIT.n##N()) UNIT##_n##N = 1.0_##UNIT##_n##N;	\
+									\
+DECLARE_MULTIPLIER_POWERS(QUANTITY, UNIT, N)
+
+#define DECLARE_POWERS(QUANTITY, UNIT)	\
+DECLARE_POWER(QUANTITY, UNIT, 0);	\
+DECLARE_POWER(QUANTITY, UNIT, 1);	\
+DECLARE_POWER(QUANTITY, UNIT, 2);	\
+DECLARE_POWER(QUANTITY, UNIT, 3);	\
+DECLARE_POWER(QUANTITY, UNIT, 4);	\
+DECLARE_POWER(QUANTITY, UNIT, 5);	\
+DECLARE_POWER(QUANTITY, UNIT, 6);	\
+DECLARE_POWER(QUANTITY, UNIT, 7);	\
+DECLARE_POWER(QUANTITY, UNIT, 8);	\
+DECLARE_POWER(QUANTITY, UNIT, 9)
+
 #define DECLARE_QUANTITY_IMPL(QUANTITY, UNIT, MULTIPLIER)		\
 constexpr QUANTITY operator "" _##UNIT(long double value) {		\
 	return QUANTITY(value * MULTIPLIER);				\
 }									\
+									\
 constexpr QUANTITY operator "" _##UNIT(unsigned long long value) {	\
 	return QUANTITY(value * MULTIPLIER);				\
 }									\
+									\
 constexpr QUANTITY UNIT = 1.0_##UNIT;					\
-DECLARE_MULTIPLIER(QUANTITY, UNIT, T,  12);				\
-DECLARE_MULTIPLIER(QUANTITY, UNIT, G,  9);				\
-DECLARE_MULTIPLIER(QUANTITY, UNIT, M,  6);				\
-DECLARE_MULTIPLIER(QUANTITY, UNIT, k,  3);				\
-DECLARE_MULTIPLIER(QUANTITY, UNIT, h,  2);				\
-DECLARE_MULTIPLIER(QUANTITY, UNIT, da, 1);				\
-DECLARE_MULTIPLIER(QUANTITY, UNIT, d, -1);				\
-DECLARE_MULTIPLIER(QUANTITY, UNIT, c, -2);				\
-DECLARE_MULTIPLIER(QUANTITY, UNIT, m, -3);				\
-DECLARE_MULTIPLIER(QUANTITY, UNIT, u, -6);				\
-DECLARE_MULTIPLIER(QUANTITY, UNIT, n, -9);				\
-DECLARE_MULTIPLIER(QUANTITY, UNIT, p, -12)
+									\
+DECLARE_MULTIPLIERS(QUANTITY, UNIT);					\
+DECLARE_POWERS(QUANTITY, UNIT)
 
 #define DECLARE_BASE_QUANTITY(QUANTITY, UNIT, MULTIPLIER)		\
 DECLARE_QUANTITY_IMPL(QUANTITY, UNIT, 1.0 / MULTIPLIER);
@@ -460,12 +618,12 @@ public:										\
 	template <class Number>							\
 	static std::string Format(Number value) {				\
 		std::ostringstream result;					\
-		result << value / (VALUE).value << detail::SPACE << #UNIT;	\
+		result << value / (VALUE).Value() << detail::SPACE << #UNIT;	\
 		return result.str();						\
 	}									\
 };										\
 typedef decltype((VALUE).withFormat<QUANTITY##Format>()) QUANTITY;		\
-DECLARE_QUANTITY_IMPL(QUANTITY, UNIT, (VALUE).value);
+DECLARE_QUANTITY_IMPL(QUANTITY, UNIT, (VALUE).Value());
 
 #define DECLARE_DERIVED_QUANTITY_NO_SYMBOL(QUANTITY, VALUE)				\
 class QUANTITY##Format {								\
@@ -477,7 +635,7 @@ public:											\
 		return result.str();							\
 	}										\
 };											\
-typedef decltype((VALUE).withFormat<QUANTITY##Format>()) QUANTITY;
+typedef decltype((VALUE).withFormat<QUANTITY##Format>()) QUANTITY;			\
 
 typedef Quantity<1, 0, 0, 0, 0, 0, 0> Length;
 DECLARE_BASE_QUANTITY(Length, m, 1.0L);
@@ -501,12 +659,13 @@ typedef Quantity<0, 0, 0, 0, 0, 0, 1> Luminosity;
 DECLARE_BASE_QUANTITY(Luminosity, cd, 1.0L);
 
 
-DECLARE_DERIVED_QUANTITY(Force, N, kg*m/s[p2]);
+DECLARE_DERIVED_QUANTITY(Force, N, kg*m/s_p2);
 DECLARE_DERIVED_QUANTITY(Energy, J, N*m);
+DECLARE_DERIVED_QUANTITY(Frequency, Hz, s_n1);
 
+DECLARE_DERIVED_QUANTITY_NO_SYMBOL(Area, m_p2);
+DECLARE_DERIVED_QUANTITY_NO_SYMBOL(Volume, m_p3);
 DECLARE_DERIVED_QUANTITY_NO_SYMBOL(Moment, N*m);
-
-DECLARE_DERIVED_QUANTITY(Test, te, km);
 
 #endif // BTUL_H
 
--- test/btul_test.cpp
+++ test/btul_test.cpp
@@ -54,63 +54,149 @@ TEST(ValueTests, test00_fundamentalQuantities) {
 	Luminosity luminosity1 = 10.0_cd;
 	Luminosity luminosity2 = 10_cd;
 
-	ASSERT_EQ(expected, length1.value);
-	ASSERT_EQ(expected, length2.value);
+	ASSERT_EQ(expected, length1.Value());
+	ASSERT_EQ(expected, length2.Value());
 
-	ASSERT_EQ(expected, mass1.value);
-	ASSERT_EQ(expected, mass2.value);
+	ASSERT_EQ(expected, mass1.Value());
+	ASSERT_EQ(expected, mass2.Value());
 
-	ASSERT_EQ(expected, time1.value);
-	ASSERT_EQ(expected, time2.value);
+	ASSERT_EQ(expected, time1.Value());
+	ASSERT_EQ(expected, time2.Value());
 
-	ASSERT_EQ(expected, current1.value);
-	ASSERT_EQ(expected, current2.value);
+	ASSERT_EQ(expected, current1.Value());
+	ASSERT_EQ(expected, current2.Value());
 
-	ASSERT_EQ(expected, temperature1.value);
-	ASSERT_EQ(expected, temperature2.value);
+	ASSERT_EQ(expected, temperature1.Value());
+	ASSERT_EQ(expected, temperature2.Value());
 
-	ASSERT_EQ(expected, amount1.value);
-	ASSERT_EQ(expected, amount2.value);
+	ASSERT_EQ(expected, amount1.Value());
+	ASSERT_EQ(expected, amount2.Value());
 
-	ASSERT_EQ(expected, luminosity1.value);
-	ASSERT_EQ(expected, luminosity2.value);
+	ASSERT_EQ(expected, luminosity1.Value());
+	ASSERT_EQ(expected, luminosity2.Value());
 }
 
 TEST(ValueTest, test01_multipliers) {
-	ASSERT_EQ(pow(10, 12), Tm.value);
-	ASSERT_EQ(pow(10, 12), (1_Tm).value);
+	ASSERT_EQ(pow(10, 12), Tm.Value());
+	ASSERT_EQ(pow(10, 12), (1_Tm).Value());
 
-	ASSERT_EQ(pow(10, 9), Gm.value);
-	ASSERT_EQ(pow(10, 9), (1_Gm).value);
+	ASSERT_EQ(pow(10, 9), Gm.Value());
+	ASSERT_EQ(pow(10, 9), (1_Gm).Value());
 
-	ASSERT_EQ(pow(10, 6), Mm.value);
-	ASSERT_EQ(pow(10, 6), (1_Mm).value);
+	ASSERT_EQ(pow(10, 6), Mm.Value());
+	ASSERT_EQ(pow(10, 6), (1_Mm).Value());
 
-	ASSERT_EQ(pow(10, 3), km.value);
-	ASSERT_EQ(pow(10, 3), (1_km).value);
+	ASSERT_EQ(pow(10, 3), km.Value());
+	ASSERT_EQ(pow(10, 3), (1_km).Value());
 
-	ASSERT_EQ(pow(10, 2), hm.value);
-	ASSERT_EQ(pow(10, 2), (1_hm).value);
+	ASSERT_EQ(pow(10, 2), hm.Value());
+	ASSERT_EQ(pow(10, 2), (1_hm).Value());
 
-	ASSERT_EQ(pow(10, 1), dam.value);
-	ASSERT_EQ(pow(10, 1), (1_dam).value);
+	ASSERT_EQ(pow(10, 1), dam.Value());
+	ASSERT_EQ(pow(10, 1), (1_dam).Value());
 
-	ASSERT_EQ(pow(10, -1), dm.value);
-	ASSERT_EQ(pow(10, -1), (1_dm).value);
+	ASSERT_EQ(pow(10, -1), dm.Value());
+	ASSERT_EQ(pow(10, -1), (1_dm).Value());
 
-	ASSERT_EQ(pow(10, -2), cm.value);
-	ASSERT_EQ(pow(10, -2), (1_cm).value);
+	ASSERT_EQ(pow(10, -2), cm.Value());
+	ASSERT_EQ(pow(10, -2), (1_cm).Value());
 
-	ASSERT_EQ(pow(10, -3), mm.value);
-	ASSERT_EQ(pow(10, -3), (1_mm).value);
+	ASSERT_EQ(pow(10, -3), mm.Value());
+	ASSERT_EQ(pow(10, -3), (1_mm).Value());
 
-	ASSERT_EQ(pow(10, -6), um.value);
-	ASSERT_EQ(pow(10, -6), (1_um).value);
+	ASSERT_EQ(pow(10, -6), um.Value());
+	ASSERT_EQ(pow(10, -6), (1_um).Value());
 
-	ASSERT_EQ(pow(10, -9), nm.value);
-	ASSERT_EQ(pow(10, -9), (1_nm).value);
+	ASSERT_EQ(pow(10, -9), nm.Value());
+	ASSERT_EQ(pow(10, -9), (1_nm).Value());
 
-	ASSERT_EQ(pow(10, -12), pm.value);
-	ASSERT_EQ(pow(10, -12), (1_pm).value);
+	ASSERT_EQ(pow(10, -12), pm.Value());
+	ASSERT_EQ(pow(10, -12), (1_pm).Value());
+}
+
+// TODO Test both orders of multiplication and division.
+TEST(ValueTest, test02_binaryArithmeticOperators) {
+	ASSERT_EQ(2_m, m + m);
+
+	ASSERT_EQ(0_m, m - m);
+
+	ASSERT_EQ(5_m, m * 5);
+	ASSERT_EQ(4_m_p2, 2_m * 2_m);
+
+	ASSERT_EQ(m, 5_m / 5);
+	ASSERT_EQ(2_m, 4_m_p2 / 2_m);
+}
+
+TEST(ValueTest, test03_arithmeticAssignmentOperators) {
+	Length l1 = m;
+	Length& l2 = l1 += m;
+	ASSERT_EQ(2_m, l1);
+	ASSERT_EQ(&l1, &l2);
+
+	Mass m1 = kg;
+	Mass& m2 = m1 -= kg;
+	ASSERT_EQ(0_kg, m1);
+	ASSERT_EQ(&m1, &m2);
+
+	Time t1 = s;
+	Time& t2 = t1 *= 5;
+	ASSERT_EQ(5_s, t1);
+	ASSERT_EQ(&t1, &t2);
+
+	Temperature T1 = 5_K;
+	Temperature& T2 = T1 /= 5;
+	ASSERT_EQ(K, T1);
+	ASSERT_EQ(&T1, &T2);
+}
+
+TEST(ValueTest, test04_unaryArithmeticOperators) {
+	Length l1 = 10.0_m;
+	Length l2 = +l1;
+	ASSERT_EQ(l1.Value(), l2.Value());
+	
+	Length l3 = -l1;
+	ASSERT_EQ(-(l1.Value()), l3.Value());
+}
+
+TEST(ValueTest, test05_prefixAndPostfixOperators) {
+	Length l1 = m;
+	Length& l2 = ++l1;
+	ASSERT_EQ(2_m, l1);
+	ASSERT_EQ(&l1, &l2);
+
+	Mass m1 = kg;
+	Mass& m2 = --m1;
+	ASSERT_EQ(0_kg, m1);
+	ASSERT_EQ(&m1, &m2);
+
+	Time t1 = s;
+	Time t2 = t1++;
+	ASSERT_EQ(2_s, t1);
+	ASSERT_EQ(s, t2);
+
+	Temperature T1 = K;
+	Temperature T2 = T1--;
+	ASSERT_EQ(0_K, T1);
+	ASSERT_EQ(K, T2);
+}
+
+TEST(ValueTest, test06_powers) {
+	Area a1 = m_p2;
+	ASSERT_EQ(1, a1.Value());
+
+	Area a2 = 10_m_p2;
+	ASSERT_EQ(10, a2.Value());
+
+	Area a3 = (10_m).p2();
+	ASSERT_EQ(pow(10, 2), a3.Value());
+
+	Area a4 = 1_km_p2;
+	ASSERT_EQ(pow(1000, 2), a4.Value());
+
+	Area a5 = 10_km_p2;
+	ASSERT_EQ(10 * pow(1000, 2), a5.Value());
+
+	Area a6 = (10_km).p2();
+	ASSERT_EQ(pow(10, 2) * pow(1000, 2), a6.Value());
 }
 
